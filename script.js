b61730434e48e84769ee9352952a58614b3ffc6c
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-18 10:08:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const container = document.querySelector('.container');
 const numSquares = 800;
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 for (let i = 0; i < numSquares; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
     square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = '#3498db'; 
+        square.style.backgroundColor = getRandomColor(); 
     });
     square.addEventListener('mouseleave', () => {
         setTimeout(() => {
