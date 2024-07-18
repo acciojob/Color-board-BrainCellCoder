c9df0ce2192271820a9cb80c890419b438e2aab6
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-18 10:06:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const container = document.querySelector('.container');
+const numSquares = 800;
+for (let i = 0; i < numSquares; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = '#3498db'; 
+    });
+    square.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            square.style.backgroundColor = '#ddd';
+        }, 1000);
+    });
+}
